a401fb9bd05395b072db3790ac39f3c484ea1957
Havva Yaşar <Havva.Yasarr@hotmail.com>
2021-06-01 02:37:17 +0300

    .

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract
+{   // T tipini kısıtlamak istiyorum
+    // referans tip olur
+    //IEntity i implemente eden bir nesne olabilir;product,costumer,category
+
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); //filtre yazabilmemizi sağlayaccak.
+
+        //filtreye göre veri getirme 
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+    }
+    }
+
+   
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity impelemente eden classlar veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
